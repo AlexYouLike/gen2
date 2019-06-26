0c0682612b963c57c8f26f849d466f7e91a602e3
Alex <Alexandra.Castillo.09@gmail.com>
2019-06-26 11:33:24 +0200

    redo of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,149 @@
-const themename = "support";
-const gulp = require( 'gulp' ),
-	autoprefixer = require( 'gulp-autoprefixer' ),
-	browserSync  = require( 'browser-sync' ).create(),
-    reload  = browserSync.reload,
-    sass  = require( 'gulp-sass' ),
-    cleanCSS  = require( 'gulp-clean-css' ),
-	sourcemaps  = require( 'gulp-sourcemaps' ),
-	concat  = require( 'gulp-concat' ),
-    imagemin  = require( 'gulp-imagemin' ),
-    changed = require( 'gulp-changed' ),
-    uglify  = require( 'gulp-uglify' ),
-    lineec  = require( 'gulp-line-ending-corrector' ),
-	pump = require('pump');
+/************ dev vs prod env ***************/
+const devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() == 'development');
 
-var root  = './' + themename + '/',
-    scss  = root + 'sass/',
-    js  = root + 'src/js/',
-    jsdist  = root + 'dist/js/';
+/************ dirs. locations ***************/
+const dir = {
+	root	: 'support/',
+	src		: 'support/src/',
+	build	: 'support/build/'
+};
 
-// Watch Files
+/**************** modules *******************/
+const gulp  		= require('gulp');
+const noop 			= require('gulp-noop');
+const imagemin 		= require('gulp-imagemin');
+const newer			= require('gulp-newer');
+const size 			= require('gulp-size');
+const sass			= require('gulp-sass');
+const postcss 		= require('gulp-postcss');
+const cleancss		= require('gulp-clean-css');
+const concat		= require('gulp-concat');
+const uglify		= require('gulp-uglify');
+const sourcemaps	= devBuild ? require('gulp-sourcemaps') : null;
+const browsersync	= devBuild ? require('browser-sync').create() : null;
+const reload		= devBuild ? browsersync.reload : null;
 
-var styleWatchFiles  = root + '**/*.scss';
 
-// Used to concat the files in a specific order.
-var jsSRC = [
-	js + 'index.js'
-  //  js + 'bootstrap.bundle.js',
-  //  js + 'bootstrap-hover.js',
-  //  js + 'nav-scroll.js',
-  //  js + 'prism.js',
-  //  js + 'resizeSensor.js',
-  //  js + 'sticky-sidebar.js',
-  //  js + 'sticky-sb.js',
-  //  js + 'skip-link-focus-fix.js'
-];
 
-// Used to concat the files in a specific order.
-var cssSRC =  [
-  root + 'src/css/*',
-  root + 'style.css',
-];
 
-var imgSRC = root + 'src/images/*',
-    imgDEST = root + 'dist/images/';
+/************* image section ***************/
+const imgConfig = {
+	src		: dir.src + 'images/**/*',
+	build	: dir.build + 'images/',
+	//	minOpts	: {
+	//		optimizationLevel : 5
+	//	},
+	minOpts : [
+		imagemin.gifsicle({interlaced:true}),
+		imagemin.jpegtran({progressive:true}),
+		imagemin.optipng({optimizationLevel:5})
+	]
+};
+
+function images() {
+	return gulp.src(imgConfig.src)				// gulp src is passed the source folder
+		.pipe(newer(imgConfig.build))			// gulp newer removes any new images already in build folder
+		.pipe(imagemin(imgConfig.minOpts))		// gulp imagemin optimizes remaining files
+		.pipe(size({ showFiles:true  }))		// gulp size reports size of processed files
+		.pipe(gulp.dest(imgConfig.build));		// files saved to gulp.dest build folder
+}
+
+
+/*************** css section ***************/
+const cssConfig = {
+	src		: dir.src + 'sass/main.scss',
+	build	: dir.build + 'css/',
+	watch	: dir.src + 'sass/**/*',
+
+	sassOpts: {
+		sourceMap		: devBuild,
+		imagePath		: '/images/',
+		outputStyle		: 'expanded',
+		precision		: 3,
+		errLogToConsole	: true
+	},
+
+	srcMaps: {
+		loadMaps	: true,
+		largeFile	: true
+	},
+
+	postCSS: [
+		require('usedcss')({				// removes unused selectors
+			html 		: ['index.html']
+		}),
+		require('postcss-assets')({			// can resolve image URL paths and info in the CSS files
+			loadPaths	: ['images/'],
+			basePath	: dir.build
+		}),
+		require('autoprefixer')({ 			// postCSS plugin adds vendor prefixes according to caniuse.com
+			browsers	: ['> 1%']
+		}),
+		require('cssnano')					// minifies resulting css file -- remove whitespace, etc...
+	]
+};
 
 function css() {
-  return gulp.src([scss + 'style.scss'])
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(sass({
-    outputStyle: 'expanded'
-  }).on('error', sass.logError))
-  .pipe(autoprefixer('last 2 versions'))
-  .pipe(sourcemaps.write())
-  .pipe(lineec())
-  .pipe(gulp.dest(root));
+	return gulp.src(cssConfig.src)
+		.pipe(sourcemaps ? sourcemaps.init({ loadMaps:true }) : noop())  // if devBuild, then sourcemaps
+		.pipe(sass(cssConfig.sassOpts).on('error', sass.logError))		 // preprocesses main.scss to css
+		.pipe(postcss(cssConfig.postCSS))								 // applies plugins in postCSS array
+		.pipe(sourcemaps ? sourcemaps.write() : noop())					 // if sourcemaps, then append to css file
+		.pipe(size({ showFiles:true  }))								 // display final size of css file
+		.pipe(gulp.dest(cssConfig.build));								 // files saved to gulp.dest build folder
 }
 
 function concatCSS() {
-  return gulp.src(cssSRC)
-  .pipe(sourcemaps.init({loadMaps: true, largeFile: true}))
-  .pipe(concat('style.min.css'))
-  .pipe(cleanCSS())
-  .pipe(sourcemaps.write('./maps/'))
-  .pipe(cleanCSS())
-  .pipe(lineec())
-  .pipe(gulp.dest(scss));
+	return gulp.src(cssConfig.build)
+		.pipe(sourcemaps ? sourcemaps.init(cssConfig.srcMaps) : noop())
+		.pipe(concat('style.min.css'))
+		.pipe(cleancss())
+		.pipe(sourcemaps ? sourcemaps.write('./maps/') : noop())
+		.pipe(cleancss())
+		.pipe(gulp.dest(cssConfig.src))
 }
 
-function javascript() {
-  return gulp.src(jsSRC)
-  .pipe(concat('support-gen.js'))
-  .pipe(uglify())
-  .pipe(lineec())
-  .pipe(gulp.dest(jsdist));
+
+/*************** js section ****************/
+const jsConfig = {
+	src		: dir.src + 'js/',
+	build	: dir.build + 'js/',
+//	watch	:
+};
+
+function javascripting() {
+	return gulp.src(jsConfig.src)
+		.pipe(concat('----'))
+		.pipe(uglify())
+		.pipe(gulp.dest(jsConfig.build));
 }
 
-function imgmin() {
-  return gulp.src(imgSRC)
-  .pipe(changed(imgDEST))
-      .pipe( imagemin([
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.optipng({optimizationLevel: 5})
-      ]))
-      .pipe( gulp.dest(imgDEST));
-}
+
+/************** watch section **************/
+const watchConfig = {
+	server	: {
+		baseDir	: './',
+		index	: 'index.html'
+	},
+	port	: 8080,
+	open	: 'external',
+//	proxy 	: 'http://localhost:8888/gen2'
+};
 
 function watch() {
-  browserSync.init({
-    open: 'external',
-    proxy: 'http://localhost:8888/gen2',
-    port: 8080,
-  });
-  gulp.watch(styleWatchFiles, gulp.series([css, concatCSS]));
-  gulp.watch(jsSRC, javascript);
-  gulp.watch(imgSRC, imgmin);
-  gulp.watch([jsdist + 'support-gen.js', scss + 'style.min.css']).on('change', browserSync.reload);
+	browsersync.init(watchConfig);
+	gulp.watch(cssConfig.watch, css)
+	gulp.watch(jsConfig.src, javascripting);
+	gulp.watch(imgConfig.src, images);
+	gulp.watch(['./index.html', jsConfig.build + 'supp-gen.js', cssConfig.watch + 'style.min.css']).on('change', browsersync.reload);
 }
 
-exports.css = css;
-exports.concatCSS = concatCSS;
-exports.javascript = javascript;
-exports.watch = watch;
-exports.imgmin = imgmin;
+/************ exports section *************/
+exports.images			= images;
+exports.css 			= gulp.series(images, css);
+exports.concatCSS		= concatCSS;
+exports.javascripting	= javascripting;
+exports.watch			= watch;
 
-var build = gulp.parallel(watch);
-gulp.task('default', build);
+const build = gulp.parallel(watch);
+gulp.task('default', build)
